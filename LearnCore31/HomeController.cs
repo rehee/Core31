68fbc5f5bff7ecb55762cb3247e44ba647d48e07
Xudong Hao <Xudong.Hao@deltafs.co.uk>
2020-04-18 15:04:45 +0100

    clean project

---
+++ LearnCore31/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using LearnCore31.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnCore31
+{
+  public class HomeController : Controller
+  {
+
+    public HomeController()
+    {
+
+    }
+    public async Task<IActionResult> Index()
+    {
+      return Content("");
+    }
+  }
+}
---
+++ LearnCore31/Services/Service1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnCore31.Services
+{
+
+  public interface IService
+  {
+    string GetThisType();
+  }
+  public class Service1<T> : IService
+  {
+    public string GetThisType()
+    {
+      return typeof(T).ToString();
+    }
+  }
+}
